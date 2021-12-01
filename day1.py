43eded750811da8ca98154db5a0e10b611f3dca3
Linnea Johansson <43148111+streckgumman@users.noreply.github.com>
2021-12-01 09:52:53 +0100

    added comments because why not

--- day1.py
+++ day1.py
@@ -1,13 +1,17 @@
 import os
 
+"""
+reads file and returns all values as an array
+"""
 def read_file(file):
     with open(file) as f:
-        #print(f)
         lines = f.readlines()
-        #print(lines)
     return [line.strip("\n") for line in lines] 
 
 
+"""
+reads some array and returns a count of how many consecutive numbers are larger
+"""
 def is_larger(input):
     count = 0
     for i in range(1,len(input)):
@@ -16,12 +20,14 @@ def is_larger(input):
     return count
 
 
+"""
+counts every three elements as one
+"""
 def count_threes(input):
     counts = []
     for i in range(len(input) -2):
         counts.append( int(input[i]) + int(input[i + 1]) + int(input[i + 2]))
     return counts
-
 
 
 if __name__ == "__main__":
